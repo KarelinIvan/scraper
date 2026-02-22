2b213a07c64cc9f4a3ede895606958d3de9d6355
KarelinIvan <ivan.karelin.1993@mail.ru>
2026-02-22 20:26:26 +0300

    3-й коммит, цикл для перебора всех карточек товара на странице

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "lxml")
 
 # Получаем карточку товара
-data = soup.find(class_="mt-auto px-1 leading-snug xl:space-y-1")
+data = soup.find_all(class_="js-load-content1")
 
-# Получаем название товара из класса html страницы
-name = data.find(class_="non_decoration text-sm font-medium xl:text-xl js-notice-block__title").text.strip()
-# С помощью метода text, выводим в консоль только текст.С помощью strip удаляем пробелы в начале и в конце сторки
-print(name)
-price = data.find(class_="font-semibold xl:text-2xl js-price-28913").text.strip()
-print(price)
+# Создаем цикл, чтобы пройтись по всем карточкам товаров на странице
+for i in data:
+    # Получаем название товара из класса html страницы
+    # С помощью метода text, выводим в консоль только текст.С помощью strip удаляем пробелы в начале и в конце сторки
+    name = i.find(class_="non_decoration text-sm font-medium xl:text-xl js-notice-block__title").text.strip()
+
+    # Получаем цену товара
+    price = i.find(class_="card-product-card__prices").text.strip()
+    # Выводим данные о статусе товара в магазине
+    product_availability = i.find(class_="product-amount_title").text
+    print(f"Наименование товара: {name} Цена: {price} Статус: {product_availability}")
